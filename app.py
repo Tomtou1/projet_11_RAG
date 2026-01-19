65898e0e001099da8f1fb38241b507de44edf991
Tomtou1 <leroythomas01@yahoo.fr>
2026-01-19 11:38:58 +0100

    first_processing_input_data

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("hello world")
+from src.read_input_data import read_and_process_inputdata
+
+if __name__ == "__main__":
+    docs = read_and_process_inputdata('data/evenements-publics-openagenda.json', debug=True)
+    print(f"Total processed documents: {len(docs)}")
+    
---
+++ src/read_input_data.py
@@ -0,0 +1,50 @@
+import json
+import pandas as pd
+from langchain_core.documents import Document
+
+def read_and_process_inputdata(file_path, debug=False):
+    # Read the json file in data
+    with open(file_path) as f:
+        raw_data = json.load(f)
+
+    #Understand the Data and view it if asked
+    if debug:
+        print(f"type of raw_data: {type(raw_data)}")
+        print(f"type of raw_data[0]: {type(raw_data[0])}") 
+        print(f"First event: {raw_data[0]}")
+        print(f"Number of events: {len(raw_data)}")
+
+
+    #Clean the Data
+    df = pd.json_normalize(raw_data) 
+    if debug: print(f"DataFrame shape: {df.shape}")
+    df = df[df['location_district'] == 'Lille-Sud']
+    if debug: print(f"DataFrame shape after region selection: {df.shape}")
+    df = df.dropna(subset=['longdescription_fr'])
+    if debug: print(f"DataFrame shape after dropping NA: {df.shape}")
+
+
+    # Feature Engineering for RAG
+    df['combined_text'] = (
+        "Title: " + df['title_fr'] + "\n" +
+        "Description: " + df['longdescription_fr'] + "\n" +
+        "Location: " + df['location_address'].fillna('') + 
+        "Date_Start: " + df['firstdate_begin'].fillna('') +
+        "Date_End: " + df['lastdate_end'].fillna('')
+    )
+
+    # Text + metadata selection
+    metadata_cols = ['uid']
+    df_final = df[['combined_text'] + metadata_cols]
+    if debug: print(f"DataFrame shape: {df_final.shape}")
+
+
+    processed_docs = []
+    for _, row in df_final.iterrows():
+        doc = Document(
+            page_content=row['combined_text'],
+            metadata={col: row[col] for col in metadata_cols}
+        )
+        processed_docs.append(doc)
+
+    return processed_docs

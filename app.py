7135bc66311123cec4544a2645bcd71bdf116d61
Tomtou1 <leroythomas01@yahoo.fr>
2026-01-19 11:46:10 +0100

    first-vectorization

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
+import os
+import faiss
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langchain.tools import tool
+from langchain.agents import create_agent
+
 from src.read_input_data import read_and_process_inputdata
+from src.vectorisation import text_split_and_vectorize
 
 if __name__ == "__main__":
-    docs = read_and_process_inputdata('data/evenements-publics-openagenda.json', debug=True)
-    print(f"Total processed documents: {len(docs)}")
     
+    # Load keys, model, embeddings, and create vector store
+    load_dotenv()
+    index_path = "vectorstore/faiss_openagenda_index"
+    model = init_chat_model("mistral-large-latest", model_provider="mistralai")
+    embeddings = MistralAIEmbeddings(model="mistral-embed")
+
+    embedding_dim = len(embeddings.embed_query("hello world"))
+    index = faiss.IndexFlatL2(embedding_dim)
+
+    vector_store = FAISS(
+        embedding_function=embeddings,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
+    )
+    
+    # Load existing vector database or create a new one
+    if os.path.exists(index_path):
+        vector_store = FAISS.load_local(index_path, embeddings, allow_dangerous_deserialization=True)
+        print(f"Loaded existing vector database from {index_path}")
+    else:
+        print(f"Creating a new vector db:")  
+        # Read / Process / Split and Vectorize data
+        processed_docs = read_and_process_inputdata('data/evenements-publics-openagenda.json', debug=True)
+        print(f"\nNumber of documents: {len(processed_docs)}")
+        print(f"\nFirst Doc: {processed_docs[0]}")
+        vector_store = text_split_and_vectorize(processed_docs, vector_store, index_path)
+        print(f"Vector database saved and ready to use.")
---
+++ src/vectorisation.py
@@ -0,0 +1,34 @@
+import os
+from langchain_text_splitters import SpacyTextSplitter
+from langchain_community.vectorstores import FAISS
+from src.read_input_data import read_and_process_inputdata
+
+
+def text_split_and_vectorize(documents, vector_store, index_path):
+    nlp_splitter = SpacyTextSplitter(chunk_size=1000, pipeline="fr_core_news_sm")
+
+    chunks = nlp_splitter.split_documents(documents)
+    print(f"Number of chunks created: {len(chunks)}")
+    
+    # Vectorize and Create FAISS index
+    vector_store.add_documents(chunks)
+    vector_store.save_local(index_path)
+    print(f"Vector database saved to {index_path}")
+    return vector_store
+
+def get_vector_store(embedding_dim, index_path):
+    if os.path.exists(index_path):
+        vector_store = FAISS.load_local(index_path, embedding_dim, allow_dangerous_deserialization=True)
+        print(f"Loaded existing vector database from {index_path}")
+    else:
+        print(f"Creating a new vector db:")  
+        # Read and process data
+        processed_docs = read_and_process_inputdata('data/evenements-publics-openagenda.json', debug=True)
+        print(f"\nNumber of documents: {len(processed_docs)}")
+        print(f"\nFirst Doc: {processed_docs[0]}")
+        
+        # Split documents into chunks and create vector database
+        vector_store = text_split_and_vectorize(processed_docs, vector_store, index_path)
+        print(f"Vector database saved and ready to use.")
+    return vector_store
+
